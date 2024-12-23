a0d1d2ce92d3aa782b9e0b6afa09e543b1929c10
Abdul Muyeed <abdulmuyeed47@gmail.com>
2024-12-23 13:30:58 +0600

    started the client 2.0

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { ChakraProvider } from '@chakra-ui/react'
 import App from './App.jsx'
-// import  './theme.js'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>

bb19b51386f48f6c2fb1ce4b1384bfaf3e4bf44b
Abdul Muyeed <abdulmuyeed47@gmail.com>
2024-12-23 11:55:55 +0600

    mongodb database connected

--- server/app.js
+++ server/app.js
@@ -1,4 +1,8 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import { connectDB } from './configs/db.js';
+
+dotenv.config();
 
 const app = express();
 
@@ -6,6 +10,9 @@ app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
-app.listen(3000, () => {
-  console.log('Server is running on http://localhost:3000');
+
+app.listen(3000, async () => {
+    await connectDB();
+  console.error('Server is running on http://localhost:3000');
+  
 });
